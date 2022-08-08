5e75a657174ba4744c5d1edf983f042e244513fb
Not-Graham <gjguzynski@gmail.com>
2022-08-07 23:30:04 -0400

    update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,9 +2,9 @@
 
 public static class ModHelperData
 {
-    public const string Name = "Round_Changer";
-    public const string Description = "Round_Changer by GrahamKraker";
+    public const string Name = "Round Changer";
+    public const string Description = "Round Changer by GrahamKraker";
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKraker";
-    public const string RepoName = "Round_Changer";
+    public const string RepoName = "BTD6-Round-Changer";
 }
