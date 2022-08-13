423cdd60a9b2fc4654f36db2dce511c658795aa4
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2022-08-13 00:26:33 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string Name = "Round Changer";
-    public const string Description = "Round Changer by GrahamKraker";
+    public const string Description = "Click on the round number to set the round";
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKraker";
     public const string RepoName = "BTD6-Round-Changer";
